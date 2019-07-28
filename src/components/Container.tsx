1308a76077ba51603b780e40908e4757d69bc9ad
Muhammad Rivki <muhammad.rivki@live.com>
2019-07-28 20:28:45 +0700

    simplify grid

--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.div`
-  margin: 0 auto;
-  max-width: 112.0rem;
-  padding: 0 2.0rem;
-  position: relative;
-  width: 100%;
-`
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -11,6 +11,14 @@ interface IColumn {
   column: 10 | 20 | 25 | 33.33 | 40 | 50 | 60 | 66.66 | 75 | 80 | 90
 }
 
+export default styled.div`
+  margin: 0 auto;
+  max-width: 112.0rem;
+  padding: 0 2.0rem;
+  position: relative;
+  width: 100%;
+`
+
 export const Row = styled.div<IRow>`
   display: flex;
   flex-direction: ${props => props.direction || 'column'};
@@ -23,6 +31,7 @@ export const Row = styled.div<IRow>`
 export const Column = styled.div<IColumn>`
   display: block;
   flex: 1 1 auto;
+  padding: 0 12px;
   margin-left: ${props => `${props.offset ? props.offset + '%' : 0}`};
   flex: ${props => `0 0 ${props.column}%`};
   max-width: ${props => `${props.column}%`};
